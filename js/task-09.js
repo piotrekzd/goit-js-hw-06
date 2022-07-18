f460f36b70ffb90c245d814c304c4273cbed67e0
Piotr Zdebski <piotrek.zdebski@gmail.com>
2022-07-18 14:47:47 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,12 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const background = document.querySelector('body');
+const colorHex = document.querySelector('.color')
+const btn = document.querySelector('.change-color');
+
+btn.addEventListener('click', (e) => {
+  colorHex.textContent = `${getRandomHexColor()}`;
+  background.style.backgroundColor = `${getRandomHexColor()}`;
+})  
