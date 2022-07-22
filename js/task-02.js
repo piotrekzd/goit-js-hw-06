c61baaf0feaa5ccbac17321f1ba647a7421ac41a
Piotr Zdebski <piotrek.zdebski@gmail.com>
2022-07-22 18:01:23 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -8,23 +8,13 @@ const ingredients = [
 ];
 
 const list = document.querySelector('#ingredients');
+const newList = [];
 
-const addIngredients = ingredients.map((element) => {
+const addIngredients = ingredients.forEach((element) => {
   const item = document.createElement('li');
   item.textContent = element;
   item.classList.add('item');
-
-  return item;
+  newList.push(item);
 });
 
-list.append(...addIngredients);
-
-// ingredients.forEach(ingredient => {
-//   const list = document.querySelector('#ingredients');
-//   const item = document.createElement('li');
-//   console.log(item);
-//   item.textContent=`${ingredient}`;
-//   item.classList.add('item');
-//   console.log(item.textContent);
-//   list.append(item);
-// });
+list.append(...newList);
